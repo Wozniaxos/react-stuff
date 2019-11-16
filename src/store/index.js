550a9ab619a473cc64d0de5b2262e449febd4183
Wozniaxos <kacperwozniax@gmail.com>
2019-11-16 19:39:35 +0100

    Provide store for app

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux'
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.text])
+    default:
+      return state
+  }
+}
+
+export default createStore(todos, ['Use Redux'])

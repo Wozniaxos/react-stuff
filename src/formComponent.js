f69497e55d0c6ef66a53d6c089839e83564edfe9
Wozniaxos <kacperwozniax@gmail.com>
2019-11-17 20:27:28 +0100

    Add support for array field

--- src/formComponent.js
+++ src/formComponent.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, FieldArray } from 'formik';
 
 const FormComponent = () => (
   <div>
     <Formik
-      initialValues={{ address: { city: '', country: ''} }}
+      initialValues={{ address: { city: '', country: ''}, items: [] }}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
@@ -12,12 +12,35 @@ const FormComponent = () => (
         }, 400);
       }}
     >
-      {({ isSubmitting }) => (
+      {({ isSubmitting, values }) => (
         <Form>
           Country
-          <Field type="address" name="address.country" />
+          <Field name="address.country" />
           City
-          <Field type="address" name="address.city" />
+          <Field name="address.city" />
+          Array Field
+          <FieldArray 
+            name="items" 
+            render={arrayHelpers => (
+              <div>
+                {values.items.map((_, index) => (
+                  <div key={index}>
+                  <Field name={`friends.${index}`} />
+                    <button
+                      type="button"
+                      onClick={() => arrayHelpers.remove(index)}
+                    >
+                      -
+                    </button>
+                  </div>
+                ))}
+
+                <button type="button" onClick={() => arrayHelpers.push('')}>
+                  Add item
+                </button>
+              </div>
+            )}
+          />
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
